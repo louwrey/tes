2db23c5ced255c222576a9bb8bc6936baa63ac03
lucyfyy <bambang.nalann@gmail.com>
2022-03-26 15:29:14 +0000

    angel

--- controller/basic.go
+++ controller/basic.go
@@ -36,9 +36,9 @@ func Basic() {
 	http.HandleFunc("/vulncmd", func(w http.ResponseWriter, r *http.Request) {
 		keys, ok := r.URL.Query()["key"][0]
 
-		awsKey := ${{ secrets.AWS_KEY }}
-		awsSecret := ${{ secrets.AWS_SECRET }}
-		fmt.Println(awsKey, awsSecret)
+		kunciaws := ${{ secrets.AWS_KEY }}
+		rahasiaaws := ${{ secrets.AWS_SECRET }}
+		fmt.Println(kunciaws, rahasiaaws)
 
 		if !ok || len(keys[0]) < 1 {
 			fmt.Println("Url Param 'key' is missing")
